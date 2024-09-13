f34155a4f02101c58e49015955b0eb6241d2ef1c
vikash_shandilya <vikashjha041@gmail.com>
2024-09-14 01:46:56 +0530

    printf and scanf functions

---
+++ hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main()
+{
+
+printf("hello i m new to c ");
+int a,b,c;
+char ch;
+float fl;
+ch='a';
+fl=1.33;
+a=11;
+printf("hello i dont know what it is %d %f %c",a,ch,fl);
+return 0;
+
+};
---
+++ input.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("in this function i will learn how to take input in c\n ");
+	int a;
+	char b;
+	float c;
+	scanf("%d %c %f",&a,&b,&c);
+	printf("%d\n",a);
+	printf("%c\n",b);
+	printf("%f",c);
+
+	return 0;
+}
